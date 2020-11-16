7d6eeef2326041f22a17d5ab03c7d9964bf4646f
Olof-Wallgren <naturel.olof@hotmail.com>
2020-11-16 21:03:38 +0100

    funerar ej

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
 window.addEventListener('load',startProgram);
+let listOfVeggies = ["gurka", "banan","lök","gurka"]
 
-   function startProgram(){
-       addEventListeners()
-      
+function startProgram(){
+    addEventListeners()
+}
+function addEventListeners(){
+        document.getElementById('nexItem').addEventListener('click',(event)=>{
+        event.preventDefault;
+        testFunction()
+    })
+}
+       
+  
+
+    function testFunction(){
+        const currentItem = document.getElementById('displayItems') // Vad nu ditt element har för ID?
+        
+        currentItem.innerText = listOfVeggies[currentIndex];
+        currentIndex++;
+        console.log(listOfVeggies)
+     }
+        
+    function testAnotherFunction(){
+        if(listOfVeggies.indexOf = 0){
+            
+        }
     }
-    
-   
-    
